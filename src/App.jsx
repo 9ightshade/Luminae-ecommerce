85e4aab6b15db52cd8428e8cf946ffc580c0f6c4
Nightshade <aniekemeusanga@gmail.com>
2023-10-25 12:30:01 +0100

    tailwind config/testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./pages/Home";
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+    return (
+        <>
+            <h1 class="text-3xl font-bold underline text-blue-200  ">Hello World!</h1>
+            <Home />
+        </>
+    );
 }
 
 export default App;
