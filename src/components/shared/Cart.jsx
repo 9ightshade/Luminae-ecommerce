247ef6cfa02636b511d81b01f3d4eedbbd9c5f05
9ightshade <aniekemeusanga@gmail.com>
2023-11-14 14:11:56 +0100

    testing

--- src/components/shared/Cart.jsx
+++ src/components/shared/Cart.jsx
@@ -5,7 +5,7 @@ export const CartComponent = () => {
             <img src={card} alt="cart" />
             <p className="hidden  md:block mx-2 text-white ">Cart</p>
             <div className="bg-green-400 text-white rounded-full w-6 h-6 flex justify-center items-center ">
-                <p>3</p>
+                <p>7</p>
             </div>
         </div>
     );
